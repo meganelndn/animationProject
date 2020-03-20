a38a9e8c461827a4ebe330dd789bac491e8abea1
megane <megane.londono@hotmail.com>
2020-03-20 13:38:37 +0100

    fetched lightbulb svg with parcel and sass

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
+"use strict";
+
+document.querySelector("DOMContentLoaded", init);
+
 /*-------------------IMPORT IMAGES------------------*/
 import {
     gallery
 } from "./modules/gallery"
 
 const data = [{
-        i: "1.png"
-    },
-    {
-        i: "2.png"
-    }
-];
+    i: "bulbWhite.svg"
+}];
 gallery(data, document.body);
+
+/*-------------------INITIALISING------------------*/
+async function init() {
+    const response = await fetch("static/imgs/timeline.svg");
+    const svgData = await response.text();
+    document.querySelector("body").innerHTML = svgData;
+    startManipulatingTheSvg();
+}
+
+function startManipulatingTheSvg() {
+
+}
