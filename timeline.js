fe5a1aa1c221e5ba5f081999de570eabed2566b0
Patricia Bettio <pfbettio@gmail.com>
2020-03-22 21:13:30 +0100

    add images svg

--- timeline.js
+++ timeline.js
@@ -1,7 +1,18 @@
 "use strict"
 
-document.querySelector("DOMContentLoaded", start);
+document.addEventListener("DOMContentLoaded", start);
 
-function start(){
-    console.log(test)
+async function start(){
+    console.log("test")
+    //timeline
+    let response = await fetch("static/svg/timeline.svg");
+    let mainSvg = await response.text();
+    document.querySelector("#mainSvg").innerHTML = mainSvg;
+  
+    //load json
+    startManipulatingData();
+}
+
+function startManipulatingData(){
+    console.log("testando")
 }
