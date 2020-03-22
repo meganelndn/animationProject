954c9b4bd7f030e131a02d16ac3653859cf7aaeb
Patricia Bettio <pfbettio@gmail.com>
2020-03-22 21:45:46 +0100

    add svg

--- timeline.js
+++ timeline.js
@@ -1,13 +1,19 @@
 "use strict"
 
 document.addEventListener("DOMContentLoaded", start);
-
+//to fix error when fetching svg using async
+const regeneratorRuntime = require("regenerator-runtime");
+ 
 async function start(){
     console.log("test")
     //timeline
     let response = await fetch("static/svg/timeline.svg");
     let mainSvg = await response.text();
     document.querySelector("#mainSvg").innerHTML = mainSvg;
+    //infobox
+    let responseBox = await fetch("/static/svg/timeline_test.svg");
+    let infoSvg = await responseBox.text();
+    document.querySelector("#infoBoxSvg").innerHTML = infoSvg;
   
     //load json
     startManipulatingData();
