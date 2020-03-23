5389f86e919750dd03d69160a933ed02393fce1c
Patricia Bettio <pfbettio@gmail.com>
2020-03-23 09:45:28 +0100

    svg images test

--- timeline.js
+++ timeline.js
@@ -1,24 +1,25 @@
+require("@babel/polyfill");
 "use strict"
 
 document.addEventListener("DOMContentLoaded", start);
 //to fix error when fetching svg using async
-const regeneratorRuntime = require("regenerator-runtime");
- 
+
 async function start(){
     console.log("test")
     //timeline
-    let response = await fetch("static/svg/timeline.svg");
+    let response = await fetch("static/imgs/svg/timeline.svg");
     let mainSvg = await response.text();
     document.querySelector("#mainSvg").innerHTML = mainSvg;
     //infobox
-    let responseBox = await fetch("/static/svg/timeline_test.svg");
+    let responseBox = await fetch("static/imgs/infobox.svg");
     let infoSvg = await responseBox.text();
     document.querySelector("#infoBoxSvg").innerHTML = infoSvg;
-  
     //load json
     startManipulatingData();
+ 
 }
 
 function startManipulatingData(){
     console.log("testando")
+    console.log()
 }
