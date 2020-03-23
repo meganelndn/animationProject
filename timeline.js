e905be3e760b749bbdd685fcae8e415723f4df57
Patricia Bettio <pfbettio@gmail.com>
2020-03-23 10:16:20 +0100

    svg fetch fixed

--- timeline.js
+++ timeline.js
@@ -7,11 +7,11 @@ document.addEventListener("DOMContentLoaded", start);
 async function start(){
     console.log("test")
     //timeline
-    let response = await fetch("static/imgs/svg/timeline.svg");
+    let response = await fetch("imgs/timeline.svg");
     let mainSvg = await response.text();
     document.querySelector("#mainSvg").innerHTML = mainSvg;
     //infobox
-    let responseBox = await fetch("static/imgs/infobox.svg");
+    let responseBox = await fetch("imgs/infobox.svg");
     let infoSvg = await responseBox.text();
     document.querySelector("#infoBoxSvg").innerHTML = infoSvg;
     //load json
