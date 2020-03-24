bdcfa33d2091cdc73020e0722e1412e44e889124
NinaAndreeaPopovici <ninandreeapopovici@gmail.com>
2020-03-24 14:08:21 +0100

    timeline js html

---
+++ timeline.js
@@ -0,0 +1,25 @@
+require("@babel/polyfill");
+"use strict"
+
+document.addEventListener("DOMContentLoaded", start);
+//to fix error when fetching svg using async
+
+async function start(){
+    console.log("test")
+    //timeline
+    let response = await fetch("imgs/timeline.svg");
+    let mainSvg = await response.text();
+    document.querySelector("#mainSvg").innerHTML = mainSvg;
+    //infobox
+    let responseBox = await fetch("imgs/infobox.svg");
+    let infoSvg = await responseBox.text();
+    document.querySelector("#infoBoxSvg").innerHTML = infoSvg;
+    //load json
+    startManipulatingData();
+ 
+}
+
+function startManipulatingData(){
+    console.log("testando")
+    console.log()
+}
