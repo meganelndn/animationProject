280f7b0697becfe13190d46db6498597bc341f1c
NinaAndreeaPopovici <ninandreeapopovici@gmail.com>
2020-03-24 08:50:51 +0100

    added all

---
+++ script2.js
@@ -0,0 +1,12 @@
+/*-------------------IMPORT IMAGES------------------*/
+import { gallery } from "./modules/gallery";
+
+const data = [
+  {
+    i: "1.png"
+  },
+  {
+    i: "2.png"
+  }
+];
+gallery(data, document.body);
